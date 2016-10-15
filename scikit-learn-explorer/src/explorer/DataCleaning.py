31f901622375f1f8d2faf6bb70241dd154e582d4
Sriharish Ranganathan <r.sriharish@gmail.com>
2016-10-15 10:40:09 -0400

    Delete DataCleaning.py

--- scikit-learn-explorer/src/explorer/DataCleaning.py
+++
@@ -1,9 +0,0 @@
-'''
-Created on Sep 18, 2016
-
-@author: Sriharish
-'''
-import re
-s = 'IBM is cognitive|IBM is "Cognitive"|ibm is enormous  company with employees with cognitive ability'
-barSplit = s.split("|")
-print(type(barSplit))
